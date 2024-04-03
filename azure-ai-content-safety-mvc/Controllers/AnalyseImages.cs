ac4d9f972df151edcf3d269549c68dbd857a6df2
Russell NMcCloy <russell.mccloy@googlemail.com>
2024-04-03 14:21:10 -0300

    Switch region and ad 2 controllers

---
+++ azure-ai-content-safety-mvc/Controllers/AnalyseImages.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using azure_ai_content_safety_mvc.Models;
+
+namespace azure_ai_content_safety_mvc.Controllers;
+
+public class AnalyseImagesController : Controller
+{
+    private readonly ILogger<AnalyseImagesController> _logger;
+
+    public AnalyseImagesController(ILogger<AnalyseImagesController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
