8c26c4836b634c09eba5437582183d8df31f12ac
Russell NMcCloy <russell.mccloy@googlemail.com>
2024-04-05 14:37:42 -0300

    Add offensive images C# upload

---
+++ UploadFilesToAzureStorageAccount/Program.cs
@@ -0,0 +1,59 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace UploadFilesToAzureStorageAccount;
+
+// NOTE: Make sure you run this first: `dotnet add package Azure.Storage.Blobs`
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Storage account and container details
+        // ============================================================================
+        string storageAccountName = "<ENTER YOUR STORAGE ACCOUNT NAME HERE>";
+        string storageAccountKey = "<ENTER YOUR STORAGE ACCOUNT KEY HERE>";
+
+        string connectionString = "DefaultEndpointsProtocol=https;AccountName=" + storageAccountName + ";AccountKey=" + storageAccountKey + ";EndpointSuffix=core.windows.net";
+        string containerName = "rdmc03-dev-images";  // The name of the container inside the storage account
+        // ============================================================================
+
+        // Enter the full, absolute path the the `offensive_images` folder
+        string folderPath = @"C:\Users\russe\code\azure-ai-content-safety\offensive_images";
+
+        // Check if the folder exists
+        if (Directory.Exists(folderPath))
+        {
+            // Get all files in the folder
+            string[] files = Directory.GetFiles(folderPath);
+
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            // Create the container if it doesn't exist
+            await containerClient.CreateIfNotExistsAsync();
+
+            // Loop through each file and upload
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+                string fileName = Path.GetFileName(file);
+                BlobClient blobClient = containerClient.GetBlobClient(Path.GetFileName(file));
+
+                // Upload the file
+                using (FileStream fileStream = File.OpenRead(file))
+                {
+                    await blobClient.UploadAsync(fileStream, true);
+                }
+
+                Console.WriteLine("File uploaded successfully.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("The specified folder does not exist.");
+        }
+
+    }
+}
--- main.tf
+++ main.tf
@@ -9,5 +9,19 @@ resource "azurerm_cognitive_account" "this" {
   location            = azurerm_resource_group.this.location
   kind                = "ContentSafety"
 
-  sku_name = "F0"
+  sku_name = local.config.azurerm_content_safety_service.sku
+}
+
+resource "azurerm_storage_account" "this" {
+  name                     = "${local.prefix_name}${local.config.azurerm_storage_account.name}"
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "RAGRS"
+}
+
+resource "azurerm_storage_container" "this" {
+  name                  = "${local.prefix_name}-${local.config.azurerm_storage_container.name}"
+  storage_account_name  = azurerm_storage_account.this.name
+  container_access_type = local.config.azurerm_storage_container.access_type # for this proof of concept the container type not private but it should be to ensure the security of your documents
 }
